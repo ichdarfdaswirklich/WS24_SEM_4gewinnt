32058a294595b79e6f1c8a9edfb0d281de8b6697
Angie Zinterl <01430236@edu.campus02.at>
2025-02-06 22:06:31 +0100

    Player Klasse Methode get_move um Input Überprüfung erweitert

--- player.py
+++ player.py
@@ -1,26 +1,57 @@
 
 
 class Player:
+    """
+    Spieler Klasse für VierGewinnt Klasse
+
+    Diese Klasse stellt das Grundgerüst für die Spielerobjekte dar.
+    """
     def __init__(self, player_id : str):
+        """
+        Initialisierung der Player Klasse
+
+        Parameters
+        ----------
+        player_id
+        """
         self.player_id = player_id
         self.current_move = 0
 
 
     def get_move(self):
         """
-        Gewünschter Zug wird von Player abgefragt
-        ________
-        Wert wird dem Attribut self.current_move übergeben, dieses wird im Game Loop auf Zug-Möglichkeit überprüft
+        Eingabeaufforderung an Spielerobjekt für gewünschte Spalte für Spielstein
+
+        Returns
+        -------
+
         """
-        current_move = input("Enter your move: (Enter '0' to exit)")
-        if "0" <= current_move <= "7":
-            self.current_move = int(current_move)
+        while True:
+            current_move = input(f"Spieler {self.player_id} Enter your move: (Enter '0' to exit)")
+            if len(current_move) == 1:
+                if "0" <= current_move <= "7":
+                    self.current_move = int(current_move)
+                    break
+            print("Ungültiger Input, try again")
 
 
 
-class PlayerAI:
-    def __init__(self):
-        pass
+
+class PlayerAI(Player):
+    """
+    PlayerAI Klasse abgeleitet von Player Klasse.
+
+    Einfache "KI" für valide Züge.
+    """
+    def __init__(self, player_id="O"):
+        """
+        Initialisierung der PlayerAI Klasse
+        Parameters
+        ----------
+        player_id
+            fixe Zuweisung von "O", da PlayerAI immer zweiter Spieler ist.
+        """
+        super().__init__(player_id)
 
     def get_move(self):
         pass
